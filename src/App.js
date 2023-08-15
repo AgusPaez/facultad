25ba3390d5396f078dc96406d03553d46d9d69e9
AgusPaez <agus.123.paez@gmail.com>
2023-08-14 22:34:01 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Cartel from './components/cartel';
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+          <Cartel/>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
